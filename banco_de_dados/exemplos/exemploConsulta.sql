8cbb9857ed6dc9dbf84e07aaff0a6294def68774
Eduardo Miguel dos Reis Teixeira <133522018+EduardoMRT@users.noreply.github.com>
2024-03-28 14:41:59 -0300

    Update exemploConsulta.sql
    Inserção de mais exemplos

--- banco_de_dados/exemplos/exemploConsulta.sql
+++ banco_de_dados/exemplos/exemploConsulta.sql
@@ -5,3 +5,30 @@ SELECT usuario.*, pessoa.*
 FROM usuario
 INNER JOIN pessoa ON usuario.ID_Pessoa = 1
 
+-- Exemplo 2
+USE Farmacia;
+  
+SELECT
+    usuario.ID_Usuario,
+    pessoa.ID_Pessoa,
+    NOME AS nomePessoa,
+    CPF,
+    produto.ID_Produto,
+    NOME_PRODUTO AS nomeProduto,
+    QUANTIDADE,
+    funcionario.ID_Funcionario
+FROM
+    item_venda
+    INNER JOIN usuario
+    INNER JOIN produto
+    INNER JOIN funcionario 
+    INNER JOIN pessoa ON item_venda.ID_Usuario = 1
+   ;
+
+-- Exemplo 3
+  SELECT
+	funcionario.ID_Funcionario,
+    pessoa.NOME
+    FROM
+	funcionario
+    INNER JOIN pessoa ON funcionario.ID_Funcionario = 1;
